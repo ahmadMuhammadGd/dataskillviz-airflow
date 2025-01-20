a60e1a1dfc53dc6243a89c6e8182c460b49ec21b
Ahmad Muhammad <121247258+ahmadMuhammadGd@users.noreply.github.com>
2025-01-20 22:10:17 +0000

    fix: jobs_dim and tags_jobs fact load logic

--- includes/mart/3-tags_jobs_fact.sql
+++ includes/mart/3-tags_jobs_fact.sql
@@ -1,21 +1,30 @@
-INSERT INTO warehouse.tags_jobs_fact (tag_id, job_id, job_title_id, seniority_id, updated_at)
-SELECT 
+INSERT INTO warehouse.tags_jobs_fact (
+    tag_id, 
+    job_id, 
+    job_title_id, 
+    seniority_id, 
+    updated_at
+)
+SELECT DISTINCT
     t.tag_id,
     g.job_id,
     ti.title_id,
     se.seniority_id,
     CURRENT_TIMESTAMP AS updated_at
 FROM 
-    staging.gsearch_jobs g
+    warehouse.jobs_dim g
+LEFT JOIN 
+    staging.gsearch_jobs sg
+    ON g.job_id = sg.job_id
 LEFT JOIN 
     warehouse.tags_dim t 
     ON t.tag = ANY(g.description_tokens)
 LEFT JOIN 
     warehouse.titles ti 
-    ON ti.title = g.cleaned_title
+    ON ti.title = sg.cleaned_title
 JOIN 
     warehouse.seniority se 
-    ON se.seniority = g.cleaned_seniority 
+    ON se.seniority = sg.cleaned_seniority 
 WHERE
     g.source_job_id IS NOT NULL
 	AND
