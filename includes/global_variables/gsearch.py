1a0abe1158d240b7c0f8f22ae329267f0e7e6259
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2025-01-21 17:52:50 +0200

    added new sql files to path section

--- includes/global_variables/gsearch.py
+++ includes/global_variables/gsearch.py
@@ -11,7 +11,8 @@ JOB_DIM_LOAD_SQL_PATH       =   "/opt/airflow/includes/mart/1-jobs_dim.sql"
 TAGS_DIM_SQL_PATH           =   "/opt/airflow/includes/mart/2-tags_dim.sql"
 TAGS_JOBS_FACT_SQL          =   "/opt/airflow/includes/mart/3-tags_jobs_fact.sql"
 SKILL_REPORT_SQL            =   "/opt/airflow/includes/mart/4-frequency_report.sql"
-
+REDUCED_TAGS_JOBS_FACT_SQL  =   "/opt/airflow/includes/mart/5-reduced_tags_jobs_fact.sql"
+FP_GROWTH_REPORT_SQL        =   "/opt/airflow/includes/mart/6-fp_growth_report.sql"
 # Links
 FASTTEXT_MODEL_URL          =   "https://github.com/ahmadMuhammadGd/skillVector-assets/raw/refs/heads/main/ft_tuned_compress_model_v02.bin"
 TAGS_LIST_URL               =   "https://github.com/ahmadMuhammadGd/skillVector-assets/raw/refs/heads/main/limited_tags_list.json"
